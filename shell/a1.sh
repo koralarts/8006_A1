63efaccc0585009ecd4e5d1a7b03e3527138c403
root <root@DataComm.(none)>
2012-01-26 14:58:39 -0800

    fixed

--- shell/a1.sh
+++ shell/a1.sh
@@ -3,6 +3,15 @@ sin=sshin
 sout=sshout
 win=wwwin
 wout=wwwout
+dport=53
+dportrange=1024:65535
+dchprange=67:68
+
+#switches
+udp="-p udp"
+tcp="-p tcp"
+d="--dport"
+s="--sport"
 
 #Setting Default Policies to DROP
 iptables -F INPUT
@@ -13,10 +22,10 @@ iptables -F OUTPUT
 iptables -P OUTPUT DROP
 
 #Allow DNS and DCHP
-iptables -A INPUT -p udp --sport 53 --dport 1024:65535 -j ACCEPT
-iptables -A OUTPUT -p udp --dport 53 --sport 1024:65535 -j ACCEPT
-iptables -A INPUT -p udp --dport 67:68 --sport 67:68 -j ACCEPT
-iptables -A OUTPUT -p udp --sport 67:68 --dport 67:68 -j ACCEPT
+iptables -A INPUT $udp $s $dport $d $dportrange -j ACCEPT
+iptables -A OUTPUT $udp $d $dport $s $dportrange -j ACCEPT
+iptables -A INPUT $udp $d $dchprange $s $dchprange -j ACCEPT
+iptables -A OUTPUT $udp $s $dchprange $d $dchprange -j ACCEPT
 
 #Create user-defined chains for accounting rules
 iptables -N $sin
@@ -25,23 +34,23 @@ iptables -N $win
 iptables -N $wout
 
 #Redirect to user-defined chains
-iptables -A INPUT -p tcp --dport ssh -j $sin
-iptables -A OUTPUT -p tcp --sport ssh -j $sout
-iptables -A INPUT -p tcp --dport www -j $win
-iptables -A OUTPUT -p tcp --sport www -j $wout
+iptables -A INPUT $tcp $d ssh -j $sin
+iptables -A OUTPUT $tcp $s ssh -j $sout
+iptables -A INPUT $tcp $d www -j $win
+iptables -A OUTPUT $tcp $s www -j $wout
 
 #Permit ssh inbound/outbound packet
-iptables -A $sin -p tcp --dport ssh -j ACCEPT
-iptables -A $sout -p tcp --dport ssh -j ACCEPT
+iptables -A $sin $tcp $d ssh -j ACCEPT
+iptables -A $sout $tcp $s ssh -j ACCEPT
 
 #Permit www inbound/outbound packet
-iptables -A $win -p tcp --dport www -j ACCEPT
-iptables -A $wout -p tcp --dport www -j ACCEPT
+iptables -A $win $tcp $d www -j ACCEPT
+iptables -A $wout $tcp $s www -j ACCEPT
 
 #Deny from sport 0-1024 with dport 80
-iptables -A $sin -p tcp --dport 80 --sport 0:1024 -j DROP
-iptables -A $win -p tcp --dport 80 --sport 0:1024 -j DROP
+iptables -A $sin $tcp $d 80 $s 0:1024 -j DROP
+iptables -A $win $tcp $d 80 $s 0:1024 -j DROP
 
 #Deny from port 0
-iptables -A $sin -p tcp --dport 0 -j DROP
-iptables -A $win -p tcp --dport 0 -j DROP
+iptables -A $sin $tcp $d 0 -j DROP
+iptables -A $win $tcp $d 0 -j DROP
