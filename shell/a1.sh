b13fd3eb05ebbadd30735a45f84fd3b46e7a0293
Karl Castillo <khcastillo@hotmail.com>
2012-01-29 12:05:55 -0800

    need to test ports

--- shell/a1.sh
+++ shell/a1.sh
@@ -4,8 +4,8 @@ sout=sshout
 win=wwwin
 wout=wwwout
 dport=53
-dportrange=1024:65535
-dchprange=67:68
+dportrange="1024:65535"
+dchprange="67:68"
 https=443
 
 #switches
@@ -27,6 +27,25 @@ iptables -N $sout
 iptables -N $win
 iptables -N $wout
 
+#Allow DNS and DCHP
+
+#DNS
+iptables -A OUTPUT $udp $s $dportrange $d $dport -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT $udp $s $dport $d $dportrange -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT $tcp $s $dportrange $d $dport -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT $tcp $s $dport $d $dportrange -m state --state ESTABLISHED -j ACCEPT
+
+#DCHP
+iptables -A INPUT $udp $d $dchprange $s $dchprange -j ACCEPT
+iptables -A OUTPUT $udp $s $dchprange $d $dchprange -j ACCEPT
+
+#Deny from sport 0-1023 with dport 80
+iptables -A INPUT $tcp $d 80 $s 0:1023 -m state --state NEW,ESTABLISHED -j DROP
+
+#Deny from port 0
+iptables -A INPUT $tcp $d 0 -m state --state NEW,ESTABLISHED -j DROP
+iptables -A INPUT $tcp $s 0 -m state --state NEW,ESTABLISHED -j DROP
+
 #Redirect to user-defined chains
 iptables -A INPUT $tcp $d ssh -j $sin
 iptables -A OUTPUT $tcp $s ssh -j $sout
@@ -40,26 +59,7 @@ iptables -A $sin $tcp $d ssh -j ACCEPT
 iptables -A $sout $tcp $s ssh -j ACCEPT
 
 #Permit www inbound/outbound packet
-iptables -A $win $tcp $s www $d $dportrange -j ACCEPT
-iptables -A $wout $tcp $s $dportrange $d www -j ACCEPT
-iptables -A $win $tcp $s $https $d $dportrange -j ACCEPT
-iptables -A $wout $tcp $s $dportrange $d $https -j ACCEPT
-
-#Allow DNS and DCHP
-
-#DNS
-iptables -A OUTPUT $udp $s $dportrange $d $dport -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT $udp $s $dport $d $dportrange -m state --state ESTABLISHED -j ACCEPT
-iptables -A OUTPUT $tcp $s $dportrange $d $dport -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT $tcp $s $dport $d $dportrange -m state --state ESTABLISHED -j ACCEPT
-
-#DCHP
-iptables -A INPUT $udp $d $dchprange $s $dchprange -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT $udp $s $dchprange $d $dchprange -m state --state NEW,ESTABLISHED -j ACCEPT
-
-#Deny from sport 0-1024 with dport 80
-iptables -A INPUT $d 80 $s 0:1024 -j DROP
-
-#Deny from port 0
-iptables -A INPUT $d 0 -j DROP
-iptables -A INPUT $s 0 -j DROP
+iptables -A $win $tcp $s www -j ACCEPT
+iptables -A $wout $tcp $d www -j ACCEPT
+iptables -A $win $tcp $s $https -j ACCEPT
+iptables -A $wout $tcp $d $https -j ACCEPT
---
+++ shell/test.sh
@@ -0,0 +1,5 @@
+echo "=================================="
+echo "Testing www using telnet"
+echo "=================================="
+echo "Command: telnet www.google.com http"
+telnet www.google.com http
